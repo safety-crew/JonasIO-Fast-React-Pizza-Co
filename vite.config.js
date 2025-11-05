13e312c60155bae110084001c7b9fc8253ba715b
safety-crew <153456442+safety-crew@users.noreply.github.com>
2025-11-05 12:47:25 +0200

    Refine deployment

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/JonasIO-Fast-react-Pizza-Co/",
+  base: "/JonasIO-Fast-React-Pizza-Co/",
 });
